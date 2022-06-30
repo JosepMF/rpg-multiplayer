2efea5197b7508527df6951467d625db5506eacd
JosepMF <jmf250207@gmail.com>
2022-06-30 20:24:37 +0200

    complete the map maker

--- src/map/MapLoader.java
+++ src/map/MapLoader.java
@@ -4,19 +4,50 @@ import main.GamePanel;
 import map.tiles.TileLoader;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MapLoader {
     GamePanel gp;
     TileLoader tileLoader;
+    int[][] mapTileNum;
 
     public MapLoader(GamePanel gp) {
         this.gp = gp;
 
         tileLoader = new TileLoader();
+        loadMap();
     }
 
     private void loadMap() {
-        
+        try {
+            InputStream is = getClass().getResourceAsStream("./res/map01.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+
+            while (col < gp.colNumber && row < gp.rowNumber) {
+                String line = br.readLine();
+
+                while(col < gp.colNumber) {
+                    String[] numbers = line.split(" ");
+                    
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+                if(col == gp.colNumber) {
+                    col = 0;
+                    row++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void draw(Graphics2D g2) {
@@ -24,14 +55,14 @@ public class MapLoader {
         int y = 0;
         int row = 0;
         int col = 0;
-        while (col < gp.colNumber*2 && row < gp.rowNumber) {
+        while (col < gp.colNumber * 2 && row < gp.rowNumber) {
             g2.setColor(tileLoader.tiles[0].color);
-            g2.fillRect(x,y,gp.squareSize,gp.squareSize);
+            g2.fillRect(x, y, gp.squareSize, gp.squareSize);
 
             x += gp.squareSize;
             col++;
 
-            if(col == gp.colNumber*2) {
+            if (col == gp.colNumber * 2) {
                 x = 0;
                 col = 0;
                 y += gp.squareSize;
