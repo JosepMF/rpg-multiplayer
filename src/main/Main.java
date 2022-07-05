1b8379216147370c8e307f6212a494931f763a9e
JosepMF <jmf250207@gmail.com>
2022-07-05 22:29:59 +0200

    position user on center the map

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,6 @@ package main;
 public class Main {
     public static void main(String[] args) {
         new GameFrame();
-        System.out.println("Hola mundos");
+        System.out.println("TODO: cambiar esto para que master no se enfade");
     }
 }
